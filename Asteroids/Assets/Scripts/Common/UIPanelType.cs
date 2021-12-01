23d6ac13febbb750e7059c9613026acb86348c58
Danila Osipov <d.osssipov@gmail.com>
2021-12-02 00:32:57 +0400

    панель с инфо игрока

--- Asteroids/Assets/Scripts/Common/UIPanelType.cs
+++ Asteroids/Assets/Scripts/Common/UIPanelType.cs
@@ -3,6 +3,7 @@
     public enum UIPanelType
     {
         LoadingPanel,
-        MainMenuPanel
+        MainMenuPanel,
+        PlayerInfoPanel
     }
 }
---
+++ Asteroids/Assets/Scripts/UI/PlayerInfoPanelView.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class PlayerInfoPanelView : UIPanelView<PlayerInfoPanelData>
+    {
+        [SerializeField] private TMP_Text _playerPosition;
+        [SerializeField] private TMP_Text _playerRotation;
+        [SerializeField] private TMP_Text _playerVelocity;
+        [SerializeField] private TMP_Text _laserShots;
+        [SerializeField] private TMP_Text _laserReloadTime;
+        
+        public override UIPanelType Type => UIPanelType.PlayerInfoPanel;
+        
+        public override void Initialize(UIPanelData data)
+        {
+        }
+        
+        protected override void UpdateView(PlayerInfoPanelData data)
+        {
+            _playerPosition.text = "Position: " + data.PlayerPosition;
+            _playerRotation.text = "Rotation: " + data.PlayerRotation;
+            _playerVelocity.text = "Velocity: " + data.PlayerVelocity;
+            _laserShots.text = "Laser shots: " + data.CurrentLaserShots / data.LaserShotsBeforeReload;
+            _laserReloadTime.text = "Laser reload time: " + data.LaserReloadTime;
+        }
+    }
+
+    public class PlayerInfoPanelData : UIPanelData
+    {
+        public Vector2 PlayerPosition { get; }
+        public Quaternion PlayerRotation { get; }
+        public Vector2 PlayerVelocity { get; }
+        public int CurrentLaserShots { get; }
+        public int LaserShotsBeforeReload { get; }
+        public float LaserReloadTime { get; }
+
+        public PlayerInfoPanelData(Vector2 playerPosition, Quaternion playerRotation,
+            Vector2 playerVelocity, int currentLaserShots, int laserShotsBeforeReload,
+            float laserReloadTime)
+        {
+            PlayerPosition = playerPosition;
+            PlayerRotation = playerRotation;
+            PlayerVelocity = playerVelocity;
+            CurrentLaserShots = currentLaserShots;
+            LaserShotsBeforeReload = laserShotsBeforeReload;
+            LaserReloadTime = laserReloadTime;
+        }
+    }
+}
